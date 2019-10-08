4ad2091fbd763fc18457e40112682169fd31ebff
Niyko <55660214+Niyko@users.noreply.github.com>
2019-10-08 23:21:24 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,6 @@
 <?php
 $DEFUALT_GITHUB_REPO = "https://github.com/Niyko/hello.git";    // <-------Default Github repo URL
 $DEFUALT_GITHUB_AUTH_KEY = "7348tr34tr834gr834rg83";            // <-------Default Github account auth key (given value is a example)
-$DEFUALT_GITHUB_EMAIL = "hello@example.com";                    // <-------Default Github account email
 $DEFUALT_GITHUB_COMMIT_MESSAGE = "First Commit";                // <-------Default commit meassage
 $DEFUALT_PASSWORD = "";                                         // <-------Add a password to PHPGit if you want to
 ?>
